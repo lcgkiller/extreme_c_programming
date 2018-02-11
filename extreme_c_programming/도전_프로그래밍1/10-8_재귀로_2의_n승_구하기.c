7db28990e1dbf1a89cce27f9aa91070f34618295
lcgkiller <lcgkiller@gmail.com>
2018-02-11 17:59:50 +0900

    10- 8 2의 n승 구하기

---
+++ extreme_c_programming/도전_프로그래밍1/10-8_재귀로_2의_n승_구하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int Recursive(int input){
+    int result = 2;
+
+    if (input==1)
+        return result;
+    
+    return result * Recursive(input-1);
+}
+
+int main(void){
+    int input;
+
+    printf("정수입력 : ");
+    scanf("%d", &input);
+    printf("2의 %d승은 %d", input, Recursive(input));
+
+    return 0;
+}
