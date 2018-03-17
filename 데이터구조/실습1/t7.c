4761fe3b5eb72b7c2c43a03842a2ecaa229298dc
lcgkiller <lcgkiller@gmail.com>
2018-03-17 23:56:55 +0900

    동적 배열 할당

---
+++ 데이터구조/실습1/t7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main(void){
+    int * arrA;
+    int * arrB;
+    int input;
+    int i;
+    int total = 0;
+
+    arrA = (int *)malloc(sizeof(int) * input);
+    arrB = (int *)malloc(sizeof(int) * input);
+
+    scanf("%d", &input); // 배열의 크기 입력
+
+    for(i=0; i<input; i++){
+        scanf("%d", &arrA[i]); // arrA 입력
+    }
+
+    for(i=0; i<input; i++){
+        scanf("%d", &arrB[i]); // arrB 입력
+    }
+
+    for(i=0; i<input; i++){
+        total = arrA[i] + arrB[input-1-i];
+        printf("%d ", total);
+        total = 0;
+    }
+
+    free(arrA);
+    free(arrB);
+
+    return 0;
+}
