d25f38f40d4928a9ca1abfeaacad4d8ea8f536dd
lcgkiller <lcgkiller@gmail.com>
2018-04-10 13:26:44 +0900

    과제3 연결리스트 구현

--- 데이터구조/과제3_연결리스트/hw2_연결리스트.c
+++ 데이터구조/과제3_연결리스트/hw2_연결리스트.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE    50
 
 typedef struct Node{
     int data;
@@ -12,6 +11,16 @@ typedef struct List{
     int size;
 }List;
 
+void initList(List *list){
+    list -> size = 0;
+    list -> head = NULL;
+}
+
+// int is_full(List *list){
+//     printf("list size [%d]\n", list->size);
+//     return (list->size) >= MAX_SIZE ? 1 : 0;
+// }
+
 void insert_node(List *list, int data){
     int i=0;
 
@@ -22,21 +31,23 @@ void insert_node(List *list, int data){
     if(list->head==NULL){ // 헤드가 비어있을 때
         list->head = new_node;
         list->size++;
+        
     }   
     else{ // 헤드가 비어있지 않을 때
         if(list->size == 1){ // 데이터 크기가 1인 경우 고려
-            if ( data < cur -> data){ // 새로 삽입 되는 데이터가 더 작은 경우 맨 앞에 삽입
+            if ( data < cur->data){ // 새로 삽입 되는 데이터가 더 작은 경우 맨 앞에 삽입
                 new_node -> next = cur; // 헤드는 다음 노드를 가리키고 있는 상태
                 list -> head = new_node; // 헤드를 교체해줌.
                 list -> size++;
                 return ;
             }
             else { // 새로 삽입되는 데이터가 더 큰 경우 뒤에 삽입
-                new_node -> next = NULL;
                 cur -> next = new_node;
                 list -> size++;
                 return ;
             }
+            cur -> next = new_node;
+            list -> size++;
         }
         else{ // 데이터 크기가 1이 아닌 경우 (즉, 2이상인 경우)
             if ( data < (cur->data) ){ // 만약 헤드보다 작다면 그냥 맨 앞에 삽입해주면 끝
@@ -47,24 +58,25 @@ void insert_node(List *list, int data){
             }
 
             for(i=0; i<list->size-1; i++){// 만약 헤드가 가리키는 값보다 크다면, 대소비교 실시
-                if (data >= cur->data && data < cur->next->data){
+                if (data >= cur->data && data < cur->next->data){ // (prev_data) <= input_data < (next_data)
                     new_node->next = cur->next;
                     cur -> next = new_node;
                     list -> size++;
                     return ;
                 }
                 else{
-                    cur = cur->next;
+                    cur = cur->next; // 자기 자리를 찾을 때까지 계속 전진
                 }
             }
             cur -> next = new_node;
             list -> size++;
         }
     }
+    
 }
 
 void display(List list){
-    int i = 0;
+    int i = 1;
     Node *p = list.head;
     for(i=0; i<list.size; i++){
         printf(" %d", p->data);
@@ -74,28 +86,21 @@ void display(List list){
 }
 int main(void){
     List list;
-    list.size = 0;
-
     int num;
     int i;
     int data;
+    int test;
+    initList(&list);
     
     scanf("%d", &num);
 
     for(i=0; i<num; i++){
         scanf("%d", &data);
-        if (i >= MAX_SIZE){
-            printf("Error. Full Exception.\n");
+        insert_node(&list, data);
+        if ((i+1)%3==0)
             display(list);
-            break;
-        }
-        else{
-            insert_node(&list, data);
-            if ((i+1)%3==0)
-                display(list);
-            if (i+1==num)
-                display(list);
-        }
+        if (i+1==num)
+            display(list);
     }
     return 0;
 }
